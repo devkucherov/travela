8c696017c3af0b0142a970ee3690e435fc6bfc87
Pasha Kucherov <dev.kucherov@gmail.com>
2023-09-04 20:42:46 +0300

    исправить скрипт

--- src/js/index.js
+++ src/js/index.js
@@ -1,2 +1,18 @@
-import mobileNav from './modules/mobile-nav.js';
-mobileNav();
+import autoComplete from '@tarekraafat/autocomplete.js';
+import countryList from './helpers/countryList';
+
+const autoCompleteJS = new autoComplete({
+  selector: '#autoCompleteCountry',
+  placeHolder: "e.g Bali, Indonesia",
+  data: {
+    src: countryList,
+  },
+  resultItem: {
+    highlight: true,
+  },
+});
+
+
+
+// import mobileNav from './modules/mobile-nav.js';
+// mobileNav();
